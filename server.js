33d374539953f7c275a9928f341109e6568ed2c6
shenvar <shenoyvarsha93@gmail.com>
2017-02-16 17:59:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var articleOne = {
    
 };
     
-    function creatTemplate (data) {
+    function createTemplate (data) {
         var title = data.title;
         var date = data.date;
         var heading=data.heading;
