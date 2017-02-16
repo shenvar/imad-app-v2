09c46c0db681d01f44be796e15d3ee10250382d7
shenvar <shenoyvarsha93@gmail.com>
2017-02-16 20:23:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,16 @@ console.log('Loaded!');
 // change the text of the main-text div
 
 var element = document.getElementById('main-text');
-element.innerHTML = 'new value'
+element.innerHTML = 'new value';
 
 // move the image
 var img = document.getElementById('madi');
+var marginLeft =0;
+function moveRight () {
+    marginLeft = marginLeft = 10;
+    img.style.marginLeft = marginLeft + 'px' ;
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
     
 };
