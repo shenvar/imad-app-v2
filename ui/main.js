fa5e96756b6e9660a346274d91b9dc5466d6b288
shenvar <shenoyvarsha93@gmail.com>
2017-02-16 20:17:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,6 @@ element.innerHTML = 'new value'
 // move the image
 var img = document.getElementById('madi');
 img.onclick = function () {
-    img.style.marginleft = '200px';
+    img.style.marginLeft = '200px';
     
 };
